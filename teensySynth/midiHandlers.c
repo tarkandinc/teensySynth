00bdcf210cd67a206151e508fc98d1a8e0de316d
tarkandinc <dinc.tarkan@gmail.com>
2021-06-03 11:34:58 +0300

    Update midiHandlers.c

---
+++ teensySynth/midiHandlers.c
@@ -0,0 +1,99 @@
+  void teensyMidiNoteOn(byte channel, byte note, byte velocity)
+  {
+
+  }
+
+  void teensyMidiNoteOff(byte channel, byte note, byte velocity)
+  {
+
+  }
+
+  void teensyMidiAfterTouchPoly(byte channel, byte note, byte velocity)
+  {
+
+  }
+
+  void teensyMidiControlChange(byte channel, byte control, byte value)
+  {
+
+  }
+
+  void teensyMidiProgramChange(byte channel, byte program)
+  {
+
+  }
+
+  void teensyMidiAfterTouch(byte channel, byte pressure)
+  {
+
+  }
+
+  void teensyMidiPitchChange(byte channel, int pitch)
+  {
+
+  }
+
+  void teensyMidiSystemExclusiveChunk(const byte *data, uint16_t length, bool last)
+  {
+
+  }
+
+  void teensyMidiSystemExclusive(byte *data, unsigned int length)
+  {
+
+  }
+
+  void teensyMidiTimeCodeQuarterFrame(byte data)
+  {
+
+  }
+
+  void teensyMidiSongPosition(uint16_t beats)
+  {
+
+  }
+
+  void teensyMidiSongSelect(byte songNumber)
+  {
+
+  }
+
+  void teensyMidiTuneRequest()
+  {
+
+  }
+
+  void teensyMidiClock()
+  {
+
+  }
+
+  void teensyMidiStart()
+  {
+
+  }
+
+  void teensyMidiContinue()
+  {
+
+  }
+
+  void teensyMidiStop()
+  {
+
+  }
+
+  void teensyMidiActiveSensing()
+  {
+
+  }
+
+  void teensyMidiSystemReset()
+  {
+
+  }
+
+  void teensyMidiRealTimeSystem(byte realtimebyte)
+  {
+      
+  }
