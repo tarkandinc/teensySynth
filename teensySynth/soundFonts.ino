a1cac22929c21463c5bb1b1fd9b262a0d1d24a2a
tarkandinc <dinc.tarkan@gmail.com>
2021-06-15 15:46:04 +0300

    Update soundFonts.ino

--- teensySynth/soundFonts.ino
+++ teensySynth/soundFonts.ino
@@ -9,38 +9,41 @@
 //Third sampleRange in 
 //Last sample data, size given in sampleCount
 
-struct sample_data soundFont;
-struct instrument_data instrument;
-uint8_t sampleRange;
-int16_t sampleCount;
-int16_t soundSample[MAX_SAMPLE_COUNT];
+struct sample_data soundFont[INSTRUMENT_COUNT];
+struct instrument_data instrument[INSTRUMENT_COUNT];
+uint8_t sampleRange[INSTRUMENT_COUNT];
+int16_t sampleCount[INSTRUMENT_COUNT];
+int16_t soundSample[INSTRUMENT_COUNT][MAX_SAMPLE_COUNT];
 
-void loadSoundFont(byte fileNo)
+void loadSoundFont(byte fileNo, byte sampleNo)
 {
-    //navigate to file
-    File rootDir = SD.open("/");
-    File sampleFile;
-    for(int i=0; i<fileNo; i++)
-      sampleFile = rootDir.openNextFile();
-    
-    //read soundFont data
-    sampleFile.read(&soundFont, sizeof(sample_data));
-    sampleFile.read(&sampleCount, sizeof(int16_t));
-    sampleFile.read(&sampleRange, sizeof(uint8_t));
-    if(sampleCount < MAX_SAMPLE_COUNT)
+    if(sampleNo < INSTRUMENT_COUNT)
     {
-      sampleFile.read(soundSample, sampleCount * sizeof(int16_t));
-      soundFont.sample = soundSample;
-      instrument.sample_count = 1;
-      instrument.sample_note_ranges = &sampleRange;
-      instrument.samples = &soundFont;
-      for(int i=0; i<5; i++)
-        wavetables[i].setInstrument((const AudioSynthWavetable::instrument_data&)instrument);
-#ifdef PRINT_MIDI_MESSAGES
-      Serial.print(sampleFile.name());
-      Serial.println(" loaded");
-#endif
-    }
-    sampleFile.close();
-    rootDir.close();
+      //navigate to file
+      File rootDir = SD.open("/");
+      File sampleFile;
+      for(int i=0; i<fileNo; i++)
+        sampleFile = rootDir.openNextFile();
+      
+      //read soundFont data
+      sampleFile.read(&soundFont[sampleNo], sizeof(sample_data));
+      sampleFile.read(&sampleCount[sampleNo], sizeof(int16_t));
+      sampleFile.read(&sampleRange[sampleNo], sizeof(uint8_t));
+      if(sampleCount[sampleNo] < MAX_SAMPLE_COUNT)
+      {
+        sampleFile.read(soundSample[sampleNo], sampleCount[sampleNo] * sizeof(int16_t));
+        soundFont[sampleNo].sample = soundSample[sampleNo];
+        instrument[sampleNo].sample_count = 1;
+        instrument[sampleNo].sample_note_ranges = &sampleRange[sampleNo];
+        instrument[sampleNo].samples = &soundFont[sampleNo];
+        for(int i=0; i<5; i++)
+          wavetables[i].setInstrument((const AudioSynthWavetable::instrument_data&)instrument[sampleNo]);
+  #ifdef PRINT_MIDI_MESSAGES
+        Serial.print(sampleFile.name());
+        Serial.println(" loaded");
+  #endif
+      }
+      sampleFile.close();
+      rootDir.close();
+   }
 }
