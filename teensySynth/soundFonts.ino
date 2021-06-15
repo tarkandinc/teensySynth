6f96f1b3721c15d33fcb4789129413784df6cd85
tarkandinc <dinc.tarkan@gmail.com>
2021-06-16 00:35:23 +0300

    Update soundFonts.ino

--- teensySynth/soundFonts.ino
+++ teensySynth/soundFonts.ino
@@ -1,49 +1,56 @@
 #include <SD.h>
 #include <Audio.h>
+#include <String.h>
 #include "soundFonts.h"
 #include "audioDesign.h"
 
 //Sound File will be formated as
-//First soundFont data as sample_data struct
-//Second sampleCount in int16_t
-//Third sampleRange in 
-//Last sample data, size given in sampleCount
+//file signature "TSSF"
+//soundFontNameSize in byte
+//soundFontName in soundFontNameSize * byte
+//soundFont data as sample_data struct
+//sampleCount in int16_t
+//sampleRange in uint8_t
+//sample data in sampleCount * int16_t
 
-struct sample_data soundFont[INSTRUMENT_COUNT];
-struct instrument_data instrument[INSTRUMENT_COUNT];
-uint8_t sampleRange[INSTRUMENT_COUNT];
-int16_t sampleCount[INSTRUMENT_COUNT];
-int16_t soundSample[INSTRUMENT_COUNT][MAX_SAMPLE_COUNT];
+struct sample_data soundFont;
+struct instrument_data instrument;
+uint8_t sampleRange;
+int16_t sampleCount;
+int16_t soundSample[MAX_SAMPLE_COUNT];
 
-void loadSoundFont(byte fileNo, byte sampleNo)
+void loadSoundFont(byte fileNo)
 {
-    if(sampleNo < INSTRUMENT_COUNT)
+    byte signature[4];
+    
+    //navigate to file
+    File rootDir = SD.open("/");
+    File sampleFile;
+    for(int i=0; i<fileNo; i++)
+      sampleFile = rootDir.openNextFile();
+    
+    //read soundFont data
+    sampleFile.read(signature, sizeof("TSSF"));
+    if(strcmp(signature,"TSSF") == 0)
     {
-      //navigate to file
-      File rootDir = SD.open("/");
-      File sampleFile;
-      for(int i=0; i<fileNo; i++)
-        sampleFile = rootDir.openNextFile();
-      
-      //read soundFont data
-      sampleFile.read(&soundFont[sampleNo], sizeof(sample_data));
-      sampleFile.read(&sampleCount[sampleNo], sizeof(int16_t));
-      sampleFile.read(&sampleRange[sampleNo], sizeof(uint8_t));
-      if(sampleCount[sampleNo] < MAX_SAMPLE_COUNT)
+      sampleFile.read(&soundFont, sizeof(sample_data));
+      sampleFile.read(&sampleCount, sizeof(int16_t));
+      sampleFile.read(&sampleRange, sizeof(uint8_t));
+      if(sampleCount < MAX_SAMPLE_COUNT)
       {
-        sampleFile.read(soundSample[sampleNo], sampleCount[sampleNo] * sizeof(int16_t));
-        soundFont[sampleNo].sample = soundSample[sampleNo];
-        instrument[sampleNo].sample_count = 1;
-        instrument[sampleNo].sample_note_ranges = &sampleRange[sampleNo];
-        instrument[sampleNo].samples = &soundFont[sampleNo];
+        sampleFile.read(soundSample, sampleCount * sizeof(int16_t));
+        soundFont.sample = soundSample;
+        instrument.sample_count = 1;
+        instrument.sample_note_ranges = &sampleRange;
+        instrument.samples = &soundFont;
         for(int i=0; i<5; i++)
-          wavetables[i].setInstrument((const AudioSynthWavetable::instrument_data&)instrument[sampleNo]);
+          wavetables[i].setInstrument((const AudioSynthWavetable::instrument_data&)instrument);
   #ifdef PRINT_MIDI_MESSAGES
         Serial.print(sampleFile.name());
         Serial.println(" loaded");
   #endif
       }
-      sampleFile.close();
-      rootDir.close();
-   }
+    }
+    sampleFile.close();
+    rootDir.close();
 }
