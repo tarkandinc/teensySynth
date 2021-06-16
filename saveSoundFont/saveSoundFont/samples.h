7accf6aaafdd265fe390c3ea7f753638e80353f7
tarkandinc <dinc.tarkan@gmail.com>
2021-06-17 01:57:19 +0300

    Create samples.h

---
+++ saveSoundFont/saveSoundFont/samples.h
@@ -0,0 +1,17 @@
+#ifndef SAMPLES_H
+#define SAPMLES_H
+
+//#include <AudioStream.h>
+#include "soundFont.h"
+
+extern struct sample_data sample;
+
+extern int16_t sampleCount;
+
+extern uint8_t range;
+
+extern struct instrument_data instrument;
+
+extern uint32_t soundSample[640];
+
+#endif
