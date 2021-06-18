b91a6a74ec32913e152bc503af54e77d5ea7c52e
tarkandinc <dinc.tarkan@gmail.com>
2021-06-18 19:46:03 +0300

    Update samples.h

--- saveSoundFont/samples.h
+++ saveSoundFont/samples.h
@@ -1,13 +1,11 @@
 #ifndef SAMPLES_H
 #define SAPMLES_H
 
-//#include <AudioStream.h>
+#include "soundSample.h"
 #include "soundFont.h"
 
 extern struct sample_data sample;
 
-extern struct instrument_data instrument;
-
-extern uint32_t soundSample[640];
+extern uint32_t soundSample[SAMPLE_COUNT];
 
 #endif
