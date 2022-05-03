eabe33bbbb56e5b4567badc3b2bebda2a6343186
tarkandinc <dinc.tarkan@gmail.com>
2022-05-04 01:36:35 +0300

    reset

--- teensySynth/audioFunctions.h
+++
@@ -1,6 +0,0 @@
-#ifndef AUDIO_FUNCTIONS_H
-#define AUDIO_FUNCTIONS_H
-
-void initAudioFunctions(void);
-
-#endif
--- teensySynth/noteLookupTable.h
+++
@@ -1,7 +0,0 @@
-#ifndef NOTE_LOOKUP_TABLE_H
-#define NOTE_LOOKUP_TABLE_H
-
-extern const float velocity2amplitude[127];
-float noteToFreqConv(byte note);
-
-#endif
